b9a119f3955778a4966f978d3f8ac06799de3dc4
david.fee <david.fee@totalmobile.co.uk>
2025-01-08 11:08:01 +0000

    update MemoryTest

--- src/test/java/MemoryTest.java
+++ src/test/java/MemoryTest.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.Home;
 
 public class MemoryTest {
+
+  ChromeDriver driver;
+
+  @BeforeEach
+  public void before() {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    driver = new ChromeDriver(options);
+    driver.get("https://www.google.com/");
+  }
+
+  @AfterEach
+  public void after() {
+    driver.quit();
+  }
+
+  @RepeatedTest(10)
+  public void memoryEater() {
+    for (int i = 0; i < 20; i++) {
+      new Home(driver);
+    }
+  }
+
 }
